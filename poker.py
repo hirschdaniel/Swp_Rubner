2887d95e06a26ead6505964d122a4a9679565ace
hirschdaniel <62133979+hirschdaniel@users.noreply.github.com>
2022-11-08 21:23:54 +0100

    Add files via upload

---
+++ poker.py
@@ -0,0 +1,133 @@
+import random
+
+import numpy as np
+
+
+def Poker():
+    c = []
+
+    for i in range(0, 52):
+        c.append(i)
+
+    cards = np.array(c)
+
+    f = 51
+    for j in range(0, 52):
+        a = random.randrange(0, 52, 1)
+        y = cards[a]
+        z = cards[f]
+        cards[f] =y
+        cards[a] = z
+        f = f - 1
+    global color
+    color = np.floor_divide(cards[1:5], 13)
+
+    global card_symbol
+    card_symbol = cards[1:5] % 13
+
+    paar()
+    drilling()
+    vierling()
+    Strasse()
+    fullHouse()
+    flush()
+    straightFlush()
+    royalFlush()
+
+
+
+
+
+
+def paar():
+    for i in range(1,6):
+        zaehler = 0
+        for j in range(1,6):
+            if card_symbol[j] == card_symbol[i]:
+                zaehler += 1
+
+        if zaehler == 2:
+            return True
+        else:
+            return False
+
+
+def drilling():
+    for i in range(1,6):
+        zaehler = 0
+        for j in range(1, 6):
+            if card_symbol[j] == card_symbol[i]:
+                zaehler += 1
+
+        if zaehler == 3:
+            return True
+        else:
+            return
+
+def vierling():
+    for i in range(1,6):
+        zaehler = 0
+        for j in range(1, 6):
+            if card_symbol[j] == card_symbol[i]:
+                zaehler += 1
+
+        if zaehler == 4:
+            return True
+        else:
+            return False
+
+
+def Strasse():
+    card_symbol_strasse = np.sort(card_symbol)[::-1]
+    strasse = card_symbol_strasse[1]
+    for i in range(2,6):
+        if card_symbol_strasse[i]<strasse:
+            strasse = strasse -1
+            zaehler = zaehler+1
+    if zaehler == 4:
+        return True
+    else:
+        return False
+
+
+def fullHouse():
+    if(drilling() == True and paar() == True ):
+        return True
+    else:
+        return False
+
+
+def flush():
+    if(color[1]== color[2] == color[3] == color[4] == color[5]):
+        return True
+    else: return False
+
+
+def straightFlush():
+    # A-K-Q-J-10
+    # 13 karten: A= 1, k=13, Q=12, J=11, 10=10
+    card_symbol_straightFlush = np.sort(card_symbol)[::-1]
+    if(card_symbol_straightFlush[1]==13 and card_symbol_straightFlush[2]==12 and card_symbol_straightFlush[3]==11 and card_symbol_straightFlush[4]==10 and card_symbol_straightFlush[5]==1):
+        return True
+    else: return False
+
+
+
+def royalFlush():
+    if(straightFlush()==True and flush()== True):
+        return True
+    else: return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
